82842d9b282d4d40f7119df4ab47af0b8e852833
luxudux <luxudux@gmail.com>
2026-06-29 16:47:59 -0600

    fix: corregir 7

--- src/main/java/com/example/demo_buildpacks/DemoBuildpacksApplication.java
+++ src/main/java/com/example/demo_buildpacks/DemoBuildpacksApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController // 1. Le decimos al Srping que esto es un API REST
 public class DemoBuildpacksApplication {
-	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBuildpacksApplication.class, args);
@@ -17,8 +17,7 @@ public class DemoBuildpacksApplication {
 
 	@GetMapping("/saludo") // 2.- Creamos la ruta web http://localhost:8080/saludo
 	public String holaMundo() {
-		return "¡Felicidades Luis! Tur proyecto de Spring Boot arrancó con éxito :).";
+		return "¡Felicidades Luis! Tur proyecto de Spring Boot arrancó con éxito :) :).";
 	}
-
 
 }
